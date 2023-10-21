96d6797a646ae6e4a6734323364212fa240fd4c2
licaiyou <345627494@qq.com>
2023-10-21 10:10:03 +0800

    fix

--- pages/contact/index/index.vue
+++ pages/contact/index/index.vue
@@ -1,9 +1,15 @@
 <template>
     <view
         class="contact_container"
+        :update="update"
+        :change:update="updateData"
     >
-        <!-- :update="update"
-        :change:update="updateData" -->
+        <div
+            id="video"
+        />
+        <div
+            id="my-video"
+        />
         <CustomNavBar
             :show-left="false" 
             is-bg-color2
@@ -66,6 +72,7 @@ export default {
     },
     data () {
         return {
+            videoelm: null,
             keyword: '',
             update: '',
             frequentContacts: []
@@ -73,9 +80,9 @@ export default {
     },
     onShow () {
         this.getFrequentContacts();
-        setTimeout(() => {
-            this.update = +new Date();
-        }, 100);
+        // setTimeout(() => {
+        //     this.update = +new Date();
+        // }, 100);
     },
     methods: {
         handleToSearch () {
@@ -99,7 +106,7 @@ export default {
 };
 </script>
 
-<!-- <script module="roomModule" lang="renderjs">
+<script module="roomModule" lang="renderjs">
 import {
     Participant,
     RemoteParticipant,
@@ -111,72 +118,103 @@ import {
 export default {
     data () {
         return {
+            trackList: [],
             room: null
         }
     },
+    // onShow () {
+    //     window.console.log(11111111111111111);
+    //     this.testOpenRoom();
+    // },
     methods: {
         updateData () {
             window.console.log(11111111111111111);
             this.testOpenRoom();
         },
-        toggleAudio: async () => {
-            if (!this.room) return;
-            const enabled = this.room.localParticipant.isMicrophoneEnabled;
-            setButtonDisabled('toggle-audio-button', true);
-            if (enabled) {
-            appendLog('disabling audio');
-            } else {
-            appendLog('enabling audio');
-            }
-            await this.room.localParticipant.setMicrophoneEnabled(!enabled);
-            setButtonDisabled('toggle-audio-button', false);
-            updateButtonsForPublishState();
-        },
+        // toggleAudio: async () => {
+        //     if (!this.room) return;
+        //     const enabled = this.room.localParticipant.isMicrophoneEnabled;
+        //     setButtonDisabled('toggle-audio-button', true);
+        //     if (enabled) {
+        //     appendLog('disabling audio');
+        //     } else {
+        //     appendLog('enabling audio');
+        //     }
+        //     await this.room.localParticipant.setMicrophoneEnabled(!enabled);
+        //     setButtonDisabled('toggle-audio-button', false);
+        //     updateButtonsForPublishState();
+        // },
 
-        toggleVideo: async () => {
-            if (!this.room) return;
-            setButtonDisabled('toggle-video-button', true);
-            const enabled = this.room.localParticipant.isCameraEnabled;
-            if (enabled) {
-            appendLog('disabling video');
-            } else {
-            appendLog('enabling video');
-            }
-            await this.room.localParticipant.setCameraEnabled(!enabled);
-            setButtonDisabled('toggle-video-button', false);
-            renderParticipant(this.room.localParticipant);
+        // toggleVideo: async () => {
+        //     if (!this.room) return;
+        //     setButtonDisabled('toggle-video-button', true);
+        //     const enabled = this.room.localParticipant.isCameraEnabled;
+        //     if (enabled) {
+        //     appendLog('disabling video');
+        //     } else {
+        //     appendLog('enabling video');
+        //     }
+        //     await this.room.localParticipant.setCameraEnabled(!enabled);
+        //     setButtonDisabled('toggle-video-button', false);
+        //     renderParticipant(this.room.localParticipant);
 
-            // update display
-            updateButtonsForPublishState();
-        },
+        //     // update display
+        //     updateButtonsForPublishState();
+        // },
         async testOpenRoom () {
             const wsURL = `ws://192.168.2.20:7880`;
             const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDAzMDM2NDIsImlzcyI6IkFQSVZWQ3BETGtaTHZSViIsIm5iZiI6MTY5NzcxMTY0Miwic3ViIjoicGFydGljaXBhbnRJZGVudGl0eTEiLCJ2aWRlbyI6eyJyb29tIjoiTVVTSyIsInJvb21Kb2luIjp0cnVlfX0._-3jkKTw-wBFJVI6BCN2bBdbEQKujeUwHVazttNCNCE`;
-            this.room = new Room({
-                // automatically manage subscribed video quality
-                adaptiveStream: true,
-
-                // optimize publishing bandwidth and CPU for published tracks
-                dynacast: true
-            });
+            this.room = new Room();
+            this.trackList = [];
             try {
                 await this.room.connect(wsURL, token);
-                window.console.log('connected to room----', this.room.name);
+                console.log('connected to room----', this.room.name);
+                const p = this.room.localParticipant;
+                await p.setCameraEnabled(true);
+                await p.setMicrophoneEnabled(true);
+                await p.enableCameraAndMicrophone();
+                const cameraTrack = p.getTrack(RemoteTrack.Source.Camera);
+                const myVideo = cameraTrack?.track?.attach();
+                const myVideoDiv = document.getElementById('my-video');
+                const videoDiv = document.getElementById('video');
+                this.room.on(RoomEvent.TrackSubscribed, (track, publication, participant) => {
+                    const { identity } = participant;
+                    const element = track.attach();
+                    const type = `${identity}-${track.kind}`;
+                    console.log(this.trackList, 'trackListtrackListtrackListtrackListtrackListtrackListtrackList----');
+                    if (this.trackList.includes(type)) {
+                        const dom = document.getElementById(type);
+                        console.log(dom, '要删除的dom');
+                        videoDiv.removeChild(dom);
+                    }
+                    element.style.width = "300px";
+                    element.style.height = "300px";
+                    element.id = type;
+                    videoDiv.appendChild(element);
+                    this.trackList.push(type);
+                    console.log(track, 'tracktracktrack');
+                    console.log('tracktracktrack', participant);
+                    console.log(publication);
+                })
+                if (!myVideoDiv.children[0]) {
+                    myVideo.style.width = "300px";
+                    myVideo.style.height = "300px";
+                    myVideo.id = 'myVideo';
+                    myVideoDiv.appendChild(myVideo);
+                }
             } catch (err) {
-                window.console.log('fail -----connected to room----', err);
+                console.log('fail -----connected to room----', err);
+                setTimeout(async () => {
+                    await this.room.connect(wsURL, token);
+                }, 1000);
             }
-            const p = this.room.localParticipant;
-            await p.enableCameraAndMicrophone();
-            // await p.setCameraEnabled(true);
-            await p.setMicrophoneEnabled(true);
-            const videoElm = document.getElementById('screenshare-video');
-            const element = RemoteTrack.attach();
-            window.console.log(element, 'elementelement');
-            videoElm.appendChild(element);
+            // console.log(uni.createVideoContext('screenshare-video'), 'tracktracktrack');
+            // window.console.log(videoElement, 'elementelement');
+            // videoelm.appendChild(element);
             // console.log(RemoteTrack.Source, '=======');
             // const screenSharePub = p.getTrack(RemoteTrack.Source.ScreenShare);
             // console.log(screenSharePub, '====999===');
-            // screenSharePub.videoTrack?.attach(videoElm);
+            // screenSharePub.videoTrack?.attach(videoelm);
             // this.room.on(RoomEvent.AudioPlaybackStatusChanged, () => {
             // console.log(this.room.canPlaybackAudio, 'this.room.canPlaybackAudiothis.room.canPlaybackAudio');
             // console.log(button, 'this.room.canPlaybackAudiothis.room.canPlaybackAudio');
@@ -194,11 +232,19 @@ export default {
         },
         beforeDestroy () {
             this.room && this.room.disconnect();
+            this.myVideo = null;
+            this.trackList = [];
         }
     }
 }
-</script> -->
+</script>
 <style lang="scss" scoped>
+
+    .screenshare-video {
+        width: 400px;
+        height: 400px;
+        border: 1px solid red;
+    }
 	.contact_container {
 		@include colBox(false);
 		height: 100vh;
