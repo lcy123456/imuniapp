03bad79886c2ad6c23afa6a032a68067bf62abb1
wzw <wangzewei_web@163.com>
2024-01-18 17:47:33 +0800

    feat: 安卓退出改为隐藏

--- App.vue
+++ App.vue
@@ -48,6 +48,24 @@ export default {
         }, 2000);
         this.thirdConfig();
         fCheckVersion();
+
+        let main = plus.android.runtimeMainActivity();
+        //为了防止快速点按返回键导致程序退出重写quit方法改为隐藏至后台
+        plus.runtime.quit = function () {
+            main.moveTaskToBack(false);
+        };
+        //重写toast方法如果内容为 ‘再按一次退出应用’ 就隐藏应用，其他正常toast
+        plus.nativeUI.toast = function (str) {
+            if (str == '再按一次退出应用') {
+                main.moveTaskToBack(false);
+                return false;
+            } else {
+                uni.showToast({
+                    title: str,
+                    icon: 'none'
+                });
+            }
+        };
     },
     async onShow() {
         this.num++;
